060936bf8cb0b01798a8555dc934416cd776660f
Ryutq <tbelezoni@gmail.com>
2025-10-02 18:32:34 -0300

    Update exercicio1a.php

--- exercicio1a.php
+++ exercicio1a.php
@@ -4,9 +4,22 @@ if (isset($_REQUEST["Enviar"])){
     $name = $_REQUEST["Cliente"];
     $sexo = $_REQUEST["sexo"];
     $valor = $_REQUEST["valorcompra"];
+
+    if ($sexo == 'feminino') {
+        $desconto = 0.20;
+        $valorFinal = $valor - ($valor * $desconto);
+        echo "Cliente: $name<br>";
+        echo "Sexo: Feminino<br>";
+        echo "Valor com 20% de desconto: R$ " . number_format($valorFinal, 2, ',', '.');
+    } else if ($sexo == 'masculino') {
+        $desconto = 0.05;
+        $valorFinal = $valor - ($valor * $desconto);
+        echo "Cliente: $name<br>";
+        echo "Sexo: Masculino<br>";
+        echo "Valor com 5% de desconto: R$ " . number_format($valorFinal, 2, ',', '.');
+    } else {
+        echo "Sexo não informado corretamente.";
+    }
 }
 
-if ($sexo = 'feminino');
-    echo "$sexo fem"
-    
 ?>
